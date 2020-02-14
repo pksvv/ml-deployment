2d7f43b8aa433d96f2c9e628ff93874502ed4a45
Vipul Gaur <gaurvipul@gmail.com>
2020-02-15 05:26:32 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Car speed should be {} mph'.format(output*50))
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+@app.route('/hello/<user>')
+def hello_name(user):
+    return render_template('hello.html',name=user)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
